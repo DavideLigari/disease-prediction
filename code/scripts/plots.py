5130372900dcbcf820db0e2fcc8e2ba4bcb2ba12
Andrea <andrea.alberti01@universitadipavia.it>
2023-12-02 19:35:53 +0100

    community analysis

--- code/scripts/plots.py
+++ code/scripts/plots.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def plotGraph(network):
@@ -77,3 +78,48 @@ def plotBipartiteGraph(network):
     plt.axis("off")
     plt.legend()
     plt.show()
+    
+    
+# -------------------------- Community plots --------------------------
+
+def top_common_entity(communities, adjacency_matrix, diseases_list, entity, top=20):
+     """
+     This function plots the top common diseases/symptoms in each community
+     :param communities: list of communities
+     :param adjacency_matrix: adjacency matrix
+     :param diseases_list: list of diseases names
+     :param entity: 'disease' or 'symptom'
+     :param top: number of diseases/symptoms to plot
+     :return: None
+     """
+
+     if entity == 'disease':
+          adjacency_matrix = adjacency_matrix.T
+     
+     num_communities = len(communities)
+     for i in range(num_communities):
+          comm = [label for index, label in enumerate(communities[i])]
+          adjacency_matrix_filtered = adjacency_matrix[:, comm]
+          avg_num = adjacency_matrix_filtered.sum(axis=1).mean()
+          num_for = adjacency_matrix_filtered.sum(axis=1)
+          num_for_sorted = np.argsort(num_for)[::-1][:top]
+          adjacency_matrix_ = adjacency_matrix_filtered[num_for_sorted, :]
+          num_for_top = adjacency_matrix_.sum(axis=1)
+
+          plotting_array = np.array([diseases_list[num_for_sorted], num_for_top])
+          plt.bar(x=plotting_array[0], height=plotting_array[1])
+          plt.plot(plotting_array[0], [avg_num] * len(num_for_top), color='red', label='Average')
+          plt.xticks(rotation=90)
+          
+          if entity == 'symptom':
+               plt.title(f'Number of symptoms for the top diseases in the community {i+1}')
+               plt.ylabel('Number of symptoms')
+               plt.xlabel('Diseases')
+          else:
+               plt.title(f'Number of diseases for the top symptoms in the community {i+1}')
+               plt.ylabel('Number of diseases')
+               plt.xlabel('Symptoms')
+               
+          plt.legend()
+          #plt.tight_layout()
+          plt.show()
