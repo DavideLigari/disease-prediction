3a5c94f4f5e4ce543bf54312a1f18a452756ef62
DavideLigari01 <davide.ligari01@universitadipavia.it>
2023-12-08 18:32:42 +0100

    added network creation

--- code/scripts/plots.py
+++ code/scripts/plots.py
@@ -23,6 +23,11 @@ def plotGraph(network):
     num_rows = len(
         [node for node, data in network.nodes(data=True) if data["bipartite"] == 0]
     )
+    node_colors = [
+        "blue" if n in range(num_rows) else "lightcoral" for n in network.nodes
+    ]
+
+    # Draw nodes
     nx.draw(
         network,
         pos,
@@ -30,10 +35,27 @@ def plotGraph(network):
         font_weight="bold",
         node_size=70,  # Adjust the node size
         width=0.15,  # Adjust the edge width
-        node_color=[
-            "blue" if n in range(num_rows) else "lightcoral" for n in network.nodes
-        ],
+        node_color=node_colors,
     )
+
+    # Add legend
+    legend_labels = {"Disease": "blue", "Symptom": "lightcoral"}
+
+    legend_elements = [
+        plt.Line2D(
+            [0],
+            [0],
+            marker="o",
+            color="w",
+            label=label,
+            markerfacecolor=color,
+            markersize=10,
+        )
+        for label, color in legend_labels.items()
+    ]
+
+    plt.legend(handles=legend_elements, loc="upper right")
+
     plt.show()
 
 
@@ -78,51 +100,57 @@ def plotBipartiteGraph(network):
     plt.axis("off")
     plt.legend()
     plt.show()
-    
-    
+
+
 # -------------------------- Community plots --------------------------
 
+
 def top_common_entity(communities, adjacency_matrix, diseases_list, entity, top=20):
-     """
-     This function plots the top common diseases/symptoms in each community
-     :param communities: list of communities
-     :param adjacency_matrix: adjacency matrix
-     :param diseases_list: list of diseases names
-     :param entity: 'disease' or 'symptom'
-     :param top: number of diseases/symptoms to plot
-     :return: None
-     """
+    """
+    This function plots the top common diseases/symptoms in each community
+    :param communities: list of communities
+    :param adjacency_matrix: adjacency matrix
+    :param diseases_list: list of diseases names
+    :param entity: 'disease' or 'symptom'
+    :param top: number of diseases/symptoms to plot
+    :return: None
+    """
 
-     comm_sizes = []
-     if entity == 'disease':
-          adjacency_matrix = adjacency_matrix.T
-     
-     num_communities = len(communities)
-     for i in range(num_communities):
-          comm = [label for index, label in enumerate(communities[i])]
-          comm_sizes.append(len(comm))
-          adjacency_matrix_filtered = adjacency_matrix[:, comm]
-          avg_num = adjacency_matrix_filtered.sum(axis=1).mean()
-          num_for = adjacency_matrix_filtered.sum(axis=1)
-          num_for_sorted = np.argsort(num_for)[::-1][:top]
-          adjacency_matrix_ = adjacency_matrix_filtered[num_for_sorted, :]
-          num_for_top = adjacency_matrix_.sum(axis=1)
+    comm_sizes = []
+    if entity == "disease":
+        adjacency_matrix = adjacency_matrix.T
 
-          plotting_array = np.array([diseases_list[num_for_sorted], num_for_top])
-          plt.bar(x=plotting_array[0], height=plotting_array[1])
-          plt.plot(plotting_array[0], [avg_num] * len(num_for_top), color='red', label='Average')
-          plt.xticks(rotation=90)
-          
-          if entity == 'symptom':
-               plt.title(f'Most common diseases in community {i+1} of size {len(comm)}')
-               plt.ylabel('Number of symptoms')
-               plt.xlabel('Diseases')
-          else:
-               plt.title(f'Most common symptoms in community {i+1} of size {len(comm)}')
-               plt.ylabel('Number of diseases')
-               plt.xlabel('Symptoms')
-               
-          plt.legend()
-          #plt.tight_layout()
-          plt.show()
-     return comm_sizes
+    num_communities = len(communities)
+    for i in range(num_communities):
+        comm = [label for index, label in enumerate(communities[i])]
+        comm_sizes.append(len(comm))
+        adjacency_matrix_filtered = adjacency_matrix[:, comm]
+        avg_num = adjacency_matrix_filtered.sum(axis=1).mean()
+        num_for = adjacency_matrix_filtered.sum(axis=1)
+        num_for_sorted = np.argsort(num_for)[::-1][:top]
+        adjacency_matrix_ = adjacency_matrix_filtered[num_for_sorted, :]
+        num_for_top = adjacency_matrix_.sum(axis=1)
+
+        plotting_array = np.array([diseases_list[num_for_sorted], num_for_top])
+        plt.bar(x=plotting_array[0], height=plotting_array[1])
+        plt.plot(
+            plotting_array[0],
+            [avg_num] * len(num_for_top),
+            color="red",
+            label="Average",
+        )
+        plt.xticks(rotation=90)
+
+        if entity == "symptom":
+            plt.title(f"Most common diseases in community {i+1} of size {len(comm)}")
+            plt.ylabel("Number of symptoms")
+            plt.xlabel("Diseases")
+        else:
+            plt.title(f"Most common symptoms in community {i+1} of size {len(comm)}")
+            plt.ylabel("Number of diseases")
+            plt.xlabel("Symptoms")
+
+        plt.legend()
+        # plt.tight_layout()
+        plt.show()
+    return comm_sizes
